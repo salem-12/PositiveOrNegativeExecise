0a38cfb68d6f23392e14bb953ca60a2f14892e22
salem-12 <salsalem02>
2020-04-14 20:33:47 -0400

    First commit

---
+++ src/PositiveOrNegativeExercise.java
@@ -0,0 +1,18 @@
+public class PositiveOrNegativeExercise {
+    public static void main(String[] args)
+    {
+    int number;
+    number=35;
+        if (number > 0)
+    {
+        System.out.println(number+" is a positive number");
+    }
+        else if(number < 0)
+    {
+        System.out.println(number+" is a negative number");
+    }
+        else {
+            System.out.println(number + " is neither positive nor negative");
+        }
+    }
+}
